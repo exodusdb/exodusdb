febe5da409b00f0890ceabf43c7017a79b51d4f9
neosys.com <neosys.com@gmail.com>
2010-09-30 19:17:07 +0000

    add new files

---
+++ exodus/exodus/exodus/mvfunctor.cpp
@@ -0,0 +1,109 @@
+/*
+Copyright (c) 2009 Stephen John Bush
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
+*/
+
+#define MV_NO_NARROW
+
+//portable shared/dynamic library macros
+//makes linux dlopen, dlsym, dlclose syntax work on windows
+//http://www.planet-source-code.com/vb/scripts/ShowCode.asp?txtCodeId=746&lngWId=3
+#if defined(_WIN32) || defined(_MSC_VER) || defined(__CYGWIN__) || defined(__MINGW32__)
+# define WIN32_LEAN_AND_MEAN
+# define _save_function_ function
+//windows.h has function defined so temp undefine and restore afterwards
+# undef function
+# include "windows.h"
+# define function _save_function_
+typedef HINSTANCE library_t;
+//# define dlopen(arg1,arg2) LoadLibrary(arg1)
+# define dlopen(arg1,arg2) LoadLibraryA(arg1)
+# define dlsym(arg1,arg2)  GetProcAddress(arg1,arg2)
+//note: MS FreeLibrary returns non-zero for success and 0 for failure
+# define dlclose(arg1) FreeLibrary(arg1)
+#else
+# include <dlfcn.h>
+  typedef void* library_t;
+#endif
+
+#include <exodus/mv.h>
+#include <exodus/mvfunctor.h>
+
+namespace exodus {
+
+ExodusFunctorBase::ExodusFunctorBase(const std::string libname,const std::string funcname)
+	: _libraryname(libname),
+	_functionname(funcname),
+	_plibrary(NULL),
+	_pfunction(NULL)
+{}
+
+ExodusFunctorBase::~ExodusFunctorBase()
+{
+	if (_plibrary!=0)
+		dlclose((library_t) _plibrary);
+}
+
+void ExodusFunctorBase::checkload()
+{
+	if (_plibrary!=0)
+		return;
+
+	//find the library or fail
+	if (not openlib())
+		throw MVException(L"Unable to load " ^ var(_libraryname) ^ L".dll/so");
+
+	//find the function or fail
+	if (not openfunc())
+		throw MVException(L"Unable to find "
+		^ var(_functionname)
+		^ L" in "
+		^ var(_libraryname)
+		^ L".dll/so");
+}
+
+bool ExodusFunctorBase::openlib()
+{
+	//open the library or return 0
+	//        println(_libraryname+EXODUSLIBEXT);
+	//dlopen arg2 is ignored by macro on windows
+#ifdef dlerror
+	dlerror();
+#endif
+
+	_plibrary=(void*) dlopen((_libraryname+EXODUSLIBEXT).c_str(), RTLD_NOW);
+
+#ifdef dlerror
+    const char *dlsym_error = dlerror();
+    if (dlsym_error)
+		var(dlsym_error).outputln();
+#endif
+	return _plibrary!=NULL;
+}
+
+bool ExodusFunctorBase::openfunc()
+{
+	//find the function and return true/false
+	//_pfunction = (EXODUSFUNCTYPE) dlsym(_plibrary, _functionname.c_str());
+	_pfunction = (void*) dlsym((library_t) _plibrary, _functionname.c_str());
+	return _pfunction!=NULL;
+}
+
+}//namespace exodus
---
+++ exodus/exodus/exodus/mvfunctor.h
@@ -0,0 +1,137 @@
+/*
+
+Traditionally, mv functions and subroutines are implemented as separately compilable units
+and there is only one function or subroutine per compilation unit (file/record).
+
+the only difference between functions and subroutines is that functions return a value whereas
+subroutines do not. Both functions and subroutines can be called with any number of arguments
+including none.
+
+exodus implements mv external functions and subroutines as C++ functors in shared libraries loaded
+on demand/just in time.
+
+=== example ===
+
+imagine we want a subroutine called "subr3" that takes one argument (and returns nothing)
+
+we decide that "subr3" will be in a library (ie a single compilable file/record) called "lib1"
+
+==== library lib1.cpp ====
+
+#include <exodus/exodus.h>
+subroutine subr3(in var1) {
+	println("lib1/subr3 says "^var1);
+}
+
+==== lib1.h ====
+
+This header file is generated AUTOMATICALLY by the command "compile lib1"
+
+==== client call1.cpp ====
+
+To use the function all we have to do is include the lib1.h file. The library
+binary is automatically loaded the first time that the function is called.
+
+#include <exodus/exodus.h>
+#include "lib1.h"
+program(){
+ subr3("xyz");
+}
+
+==== sample session ====
+
+compile lib1
+compile call1
+call1
+lib1/sub3 says xyz
+
+=== Exodus Library Concept ===
+
+exodus allows multiple functions and subroutines in a particular file using the following syntax
+
+eg in a file lib1.cpp
+
+#include <exodus/exodus.h>
+function func1(args...) {
+...
+}
+subroutine sub1(args...) {
+...
+}
+
+To use and call the above functions in a program or another library you just need to include the header file
+which is automatically generated when you do "compile lib1"
+
+#include "lib1.h"
+
+Alternatively, to keep it simple and just like traditional multivalue keep each function
+and subroutine in a separate file with the same name as the function or subroutine.
+That way you can forget the new exodus concept of "library" which is not a traditional mv concept
+
+*/
+
+#ifndef MVFUNCTOR_H
+#define MVFUNCTOR_H
+
+#if defined(_WIN32) || defined(_MSC_VER) || defined(__CYGWIN__) || defined(__MINGW32__)
+# define EXODUSLIBEXT ".dll"
+#else
+# define EXODUSLIBEXT ".so"
+#endif
+
+namespace exodus {
+
+class DLL_PUBLIC ExodusFunctorBase
+{
+
+public:
+
+ExodusFunctorBase(const std::string libname,const std::string funcname);
+
+virtual ~ExodusFunctorBase();
+//private:
+protected:
+
+//use void* to speed compilation of exodus applications on windows by avoiding
+//inclusion of windows.h here. In the implementation cast to HINSTANCE and 
+(void*) _plibrary;
+(void*) _pfunction;
+
+std::string _libraryname;
+std::string _functionname;
+
+void checkload();
+bool openlib();
+bool openfunc();
+
+};
+
+/*
+#define EXODUSFUNCNAME subr3
+#define EXODUSLIBNAME lib1
+#define EXODUSFUNCNAME subr3
+#define EXODUSFUNCRETURN void
+#define EXODUSFUNCARGS in arg1
+#define EXODUSFUNCARGS2 arg1
+#define EXODUSFUNCTORCLASSNAME ExodusFunctor_subr3
+#define EXODUSFUNCTYPE ExodusDynamic_subr3
+#define EXODUSLIBNAMEQQ "lib1"
+#define EXODUSFUNCNAMEQQ "subr3"
+#define EXODUSFUNCRETURNVOID 1
+#include <exodus/mvlink.h>
+#undef EXODUSLIBNAME
+#undef EXODUSFUNCNAME
+#undef EXODUSFUNCRETURN
+#undef EXODUSFUNCARGS
+#undef EXODUSFUNCARGS2
+#undef EXODUSFUNCTORCLASSNAME
+#undef EXODUSFUNCTYPE
+#undef EXODUSLIBNAMEQQ
+#undef EXODUSFUNCNAMEQQ
+#undef EXODUSCLASSNAME
+#undef EXODUSFUNCRETURNVOID
+*/
+
+}//namespace exodus
+
+#endif //MVFUNCTOR_H
