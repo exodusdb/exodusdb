62942821c65b7505e76b078ee74637727a6899bf
alex.nenko <alex.nenko@415b895b-543e-5d66-3bb8-6f700c98dabf>
2010-12-01 12:18:18 +0000

    new files to support multiple connections per thread

---
+++ trunk/exodus/exodus/exodus/mvdbconns.cpp
@@ -0,0 +1,77 @@
+//
+// Exodus -
+//
+// mvdbconn.cpp - keep table of connections in the way, that connection is linked to 'filename'
+//		variable (that is linked to SQL TABLE name - in mvint field)
+//
+#include "mv.h"
+#define INSIDE_MVDBCONNS_CPP
+#include "mvdbconns.h"
+#include <boost/thread/mutex.hpp>
+boost::mutex mvconnections_mutex;
+
+namespace exodus {
+
+MvConnectionsCache::MvConnectionsCache( DELETER_AND_DESTROYER del_)
+	: connection_id( 0)
+	, del( del_)
+{}
+
+//MvConnectionsCache::set_deleter( DELETER_AND_DESTROYER del_)
+//{
+//	del = del_;
+//}
+
+int MvConnectionsCache::add_connection( CACHED_CONNECTION conn_to_cache)
+{
+	boost::mutex::scoped_lock lock(mvconnections_mutex);
+
+	connection_id++;
+	tbl[connection_id] = conn_to_cache;
+	return connection_id;
+}
+
+CACHED_CONNECTION MvConnectionsCache::get_connection( int index) const
+{
+	boost::mutex::scoped_lock lock(mvconnections_mutex);
+	CONN_MAP::const_iterator iter = tbl.find( index);
+	return ( CACHED_CONNECTION)( iter == tbl.end() ? 0 : iter->second);
+}
+
+CACHED_CONNECTION MvConnectionsCache::get_current_connection() const
+{
+	boost::mutex::scoped_lock lock(mvconnections_mutex);
+	CONN_MAP::const_iterator iter = tbl.find( connection_id);
+	return ( CACHED_CONNECTION)( iter == tbl.end() ? 0 : iter->second);
+}
+
+void MvConnectionsCache::del_connection( int index)
+{
+	boost::mutex::scoped_lock lock(mvconnections_mutex);
+	CACHED_CONNECTION p /*std::pair<int, void*> p*/ = (CACHED_CONNECTION) tbl.find( index)->second;
+	del( p);
+	tbl.erase( index);
+}
+
+int MvConnectionsCache::get_current_id() const
+{
+	boost::mutex::scoped_lock lock(mvconnections_mutex);
+	return connection_id; 
+}
+
+MvConnectionsCache::~MvConnectionsCache()
+{
+	boost::mutex::scoped_lock lock(mvconnections_mutex);
+	CONN_MAP::iterator ix;
+
+/*ALN:TODO: make following code not to crash
+	Seems that after 2 threads closed, PQfinish( PGconn *) throws exception,
+	may be because PGconn * pointer is invalidated already.
+	Investigate this.
+    May be use PQstatus(PGconn *)
+*/
+	for( ix = tbl.begin(); ix != tbl.end(); ix ++)
+		del(( CACHED_CONNECTION) ix->second);
+}
+
+}	// namespace
---
+++ trunk/exodus/exodus/exodus/mvdbconns.h
@@ -0,0 +1,63 @@
+//
+// Exodus -
+//
+// mvdbconn.h - keep table of connections in the way, that connection is linked to 'filename'
+//		variable (that is linked to SQL TABLE name - in mvint field)
+//
+// Requirement 1. Current default connection: PGconn* thread_pgconn=tss_pgconns.get();
+// Requirement 2. When open( filename) - if filename is 'only' STR variable, store current connection within
+// Requirement 3. read/write: if filename is 'only' STR variable, store current connection within
+// Requirement 4. 'filename' variable with stored connection has special mvtyp = MVTYPE_SQLOPENED
+// Requirement 5. NO ! There is connection table, where all connection variables should be registered (at 2 and 3)
+// Requirement 6. No action is performed in destructor of 'filename' variable
+// Requirement 7. disconnect() without parameters closes current connection
+// Requirement 8. filename.disconnect() closes the connection, linked to the 'filename', and 'frees' this var
+// Requirement 9. Closed connection: been closed and marked as 'invalidated'
+// Requirement 10. Attempt to use 'invalidated' connection raises exception
+// Requirement 11. 
+
+#ifndef MVDBCONNS_H
+#  define MVDBCONNS_H
+
+#include <map>
+#include <libpq-fe.h>//in postgres/include
+
+namespace exodus {
+
+typedef PGconn * CACHED_CONNECTION;
+typedef void ( * DELETER_AND_DESTROYER )( CACHED_CONNECTION);
+typedef std::map<int, void *> CONN_MAP;
+
+#define CONNECTION_ENTRY_FREE  (0)
+#define ACTIVE_CONNECTION      (1)
+#define INVALIDATED_CONNECTION (-1)
+
+//ALN:TODO: remove MvConnectionEntry
+class MvConnectionEntry		// used as 'second' in pair, stored in connection map
+{
+  public:
+	int flag;	// =0 means that slot is empty
+	CACHED_CONNECTION connection;
+	int extra;
+};
+
+class MvConnectionsCache
+{
+  public:
+	MvConnectionsCache( DELETER_AND_DESTROYER del_);
+	int add_connection( CACHED_CONNECTION connection_with_file);
+	CACHED_CONNECTION get_connection( int index) const;
+	CACHED_CONNECTION get_current_connection() const;
+	int get_current_id() const;
+	void del_connection( int index);
+	virtual ~MvConnectionsCache();
+
+  private:
+	DELETER_AND_DESTROYER del;
+	int connection_id;
+//	std::map<MvConnectionEntry> __tbl__;//
+	CONN_MAP tbl;
+};
+
+}	// namespace
+#endif // MVDBCONNS_H
