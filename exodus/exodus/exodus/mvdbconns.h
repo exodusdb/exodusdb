ad4f59fb9210208ff64d5234daa4737bb1268c06
alex.nenko <alex.nenko@415b895b-543e-5d66-3bb8-6f700c98dabf>
2010-12-07 13:24:34 +0000

    lock tables are placed in connection cache table. Bug in createtable fixed.

--- exodus/exodus/exodus/mvdbconns.h
+++ exodus/exodus/exodus/mvdbconns.h
@@ -23,24 +23,65 @@
 #include <libpq-fe.h>//in postgres/include
 #include <boost/thread/mutex.hpp>
 
+// INtroducing the type for LockTable, it should be unordered set
+
+//#define HAVE_TR1
+#ifdef HAVE_CONFIG_H
+#include "config.h"
+#endif
+#ifdef  HAVE_CXX0X
+#  include <unordered_set>
+#  define UNORDERED_SET_FOR_LOCKTABLE std::unordered_set<uint64_t>
+
+//expect bug here since centos5.3 32bit has tr1 but not with an unordered set
+#elif defined(HAVE_TR1)
+#  include <tr1/unordered_set>
+#  define UNORDERED_SET_FOR_LOCKTABLE std::tr1::unordered_set<uint64_t>
+
+//assume we have a recent boost on windows
+//boost 1.33 doesnt seem to have unordered set
+#elif defined(HAVE_BOOST_UNORDERED_SET) || defined(_MSC_VER)
+#  include <boost/unordered_set.hpp>
+#  define UNORDERED_SET_FOR_LOCKTABLE boost::unordered_set<uint64_t>
+#else
+#  define USE_MAP_FOR_UNORDERED
+#  include <map>
+#  define UNORDERED_SET_FOR_LOCKTABLE std::map<uint64_t,int>
+#endif
+
+#include "MurmurHash2_64.h"
+
+//typedef boost::unordered_set<uint64_t> LockTable; 
+//typedef UNORDERED_SET_FOR_LOCKTABLE<uint64_t> LockTable;
+typedef UNORDERED_SET_FOR_LOCKTABLE LockTable;
+//typedef tr1::unordered_set<uint64_t> LockTable;
+
 namespace exodus {
 
+//#define CONNECTION_ENTRY_FREE  (0)
+//#define ACTIVE_CONNECTION      (1)
+//#define INVALIDATED_CONNECTION (-1)
+
 typedef PGconn * CACHED_CONNECTION;
-typedef void ( * DELETER_AND_DESTROYER )( CACHED_CONNECTION);
-typedef std::map<int, void *> CONN_MAP;
+typedef void ( * DELETER_AND_DESTROYER )( CACHED_CONNECTION/*, UNORDERED_SET_FOR_LOCKTABLE * */);
 
-#define CONNECTION_ENTRY_FREE  (0)
-#define ACTIVE_CONNECTION      (1)
-#define INVALIDATED_CONNECTION (-1)
-
-//ALN:TODO: remove MvConnectionEntry
 class MvConnectionEntry		// used as 'second' in pair, stored in connection map
 {
   public:
-	int flag;	// =0 means that slot is empty
+	int flag;
 	CACHED_CONNECTION connection;
+	LockTable * plock_table;
 	int extra;
+
+	MvConnectionEntry()
+		: flag(0), connection(0), plock_table( 0), extra(0)
+	{}
+	MvConnectionEntry( CACHED_CONNECTION connection_, UNORDERED_SET_FOR_LOCKTABLE * LockTable_)
+		: flag(0), connection(connection_), plock_table( LockTable_), extra(0)
+	{}
 };
+
+typedef std::map<int, MvConnectionEntry> CONN_MAP;
 
 class MvConnectionsCache
 {
@@ -48,8 +89,9 @@ class MvConnectionsCache
 	MvConnectionsCache( DELETER_AND_DESTROYER del_);
 	int add_connection( CACHED_CONNECTION connection_with_file);
 	CACHED_CONNECTION get_connection( int index) const;
-	CACHED_CONNECTION get_current_connection() const;
-	int get_current_id() const;
+	UNORDERED_SET_FOR_LOCKTABLE * get_lock_table( int index) const;
+	CACHED_CONNECTION _get_current_connection() const;
+	int _get_current_id() const;
 	void del_connection( int index);
 	virtual ~MvConnectionsCache();
 
