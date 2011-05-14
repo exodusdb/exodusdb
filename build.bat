d2be62136428657ecdbf593818be20142216c5c8
neosys.com <neosys.com@gmail.com>
2011-05-14 20:13:05 +0000

    windows packaging script updated to work with SDK only build environment - tested on win2008x64r2sp1. exodus_cli no longer built as exodus - to avoid conflict with exodus lib build intermediaries like exodus.obj

--- build.bat
+++ build.bat
@@ -3,30 +3,8 @@ rem COPY AND MODIFY THIS FILE TO SUIT YOUR OWN ENVIRONMENT
 rem make a shortcut to the copy so you can quickly develop and build
 rem with the right environment variables set
 
-rem ----- BOOST32 -----
 
-rem uncomment if you have binaries here but best to build from scratch to avoid 0xc0150002 error
-rem set BOOST32=C:\Program Files\Boost\boost_1_46_1
-
-rem assuming we have built libs here (in stage32)
-set BOOST32=d:\boost_1_46_1
-
-rem ----- BOOST64 -----
-
-rem binary installers are not available so we have built boost x64 libs (in stage64)
-set BOOST64=D:\boost_1_46_1
-
-rem ----- POSTGRESQL32 -----
-
-rem on Win32, postgres is installed here
-set POSTGRESQL32=C:\Program Files\PostgreSQL\9.0
-
-rem but on win/64, postgres is installed here
-rem set POSTGRESQL32=C:\Program Files (x86)\PostgreSQL\9.0
-
-rem ----- POSTGRESQL64 -----
-
-set POSTGRESQL64=C:\Program Files\PostgreSQL\9.0
+call config.bat
 
 rem ----- BUILD AT CONSOLE OR IN VISUAL STUDIO -----
 
