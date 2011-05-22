d0dc5bff424b664024750716f74d9b32ec02aae6
neosys.com@gmail.com <neosys.com@gmail.com@415b895b-543e-5d66-3bb8-6f700c98dabf>
2011-05-22 20:56:48 +0000

    compile defaults to release to avoid win crash on external subroutines

--- trunk/dev.cmd
+++ trunk/dev.cmd
@@ -1,3 +1,7 @@
 call config DEV > config.log
+@echo BOOST32=%BOOST32%
+@echo BOOST64=%BOOST64%
+@echo POSTGRES32=%POSTGRES32%
+@echo POSTGRES64=%POSTGRES64%
 %EXO_DEVCMD% %EXO_DEVOPT%
 if "%EXO_BATCHMODE%" == "" pause
