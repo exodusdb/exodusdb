fca6fdf45b08561a034e41c06d8e6b540a27038f
neosys.com <neosys.com@415b895b-543e-5d66-3bb8-6f700c98dabf>
2011-06-15 00:43:29 +0000

    php python and perl modules built and tested on Ubuntu64

--- trunk/swig/test.py
+++ trunk/swig/test.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python
+print "Started\n"
 from exodus import *
 print var().date().oconv(var("D"))
 print var().time().oconv(var("MT"))
 print var().createfile(var("tempfilepython"),var(""))
+print "Finished"
