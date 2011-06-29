d3c119e1c4e12332aa033a029a89a49264c0e7f2
neosys.com <neosys.com@415b895b-543e-5d66-3bb8-6f700c98dabf>
2011-06-29 17:01:21 +0000

    addrepo_lucid.sh

---
+++ trunk/addrepo_lucid.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+sudo sh -c 'echo "deb http://ppa.launchpad.net/steve-bush/ppa-exodus/ubuntu lucid main\n \
+ deb-src http://ppa.launchpad.net/steve-bush/ppa-exodus/ubuntu lucid main" \
+ > /etc/apt/sources.list.d/exodus-ppa-lucid.list'
+
+sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys E7815451
+
