cf901c35c3ea36cb79459579fbaaf0a4feb4a316
neosys.com@gmail.com <neosys.com@gmail.com@415b895b-543e-5d66-3bb8-6f700c98dabf>
2011-06-30 10:37:59 +0000

    polish addrepo_lucid.sh

--- trunk/addrepo_lucid.sh
+++ trunk/addrepo_lucid.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 set -ex
 
-#Add a Launchpad PPA repository hosted by Canonical
+#Trust GNUPG Key E7815451 https://launchpad.net/~steve-bush/+archive/ppa-exodus
+sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys E7815451
+
+#Add a Launchpad PPA repository hosted by Canonical for the above key
 sudo sh -c 'echo "deb http://ppa.launchpad.net/steve-bush/ppa-exodus/ubuntu lucid main\n \
  deb-src http://ppa.launchpad.net/steve-bush/ppa-exodus/ubuntu lucid main" \
  > /etc/apt/sources.list.d/exodus-ppa-lucid.list'
-
-#Trust GNUPG Key E7815451 https://launchpad.net/~steve-bush/+archive/ppa-exodus
-sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys E7815451
 
 sudo apt-get update
 
