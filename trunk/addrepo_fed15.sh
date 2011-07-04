f24b6987f0cbdec0306ee4af93278c9604fe2d9c
neosys.com <neosys.com@415b895b-543e-5d66-3bb8-6f700c98dabf>
2011-07-04 19:35:39 +0000

    addrepo_rh5.sh addrepo_fed15.sh

---
+++ trunk/addrepo_fed15.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+set -ex
+sudo su -c 'echo "
+[home_neosys]
+[home_neosys]
+name=neosys Home Project Fedora_15
+type=rpm-md
+baseurl=http://download.opensuse.org/repositories/home:/neosys/Fedora_15/
+gpgcheck=1
+gpgkey=http://download.opensuse.org/repositories/home:/neosys/Fedora_15/repodata/repomd.xml.key
+enabled=1
+">/etc/yum.repos.d/openbuildservice-exodus.repo'
