b4b39a6782dfa5306d35b7dc3e8faf62cd65d6f9
neosys.com <neosys.com@415b895b-543e-5d66-3bb8-6f700c98dabf>
2014-05-08 21:44:03 +0000

    builds and installs on ubuntu 14.04 with dev postgres 9.4 configured as ./configure --prefix=/usr/local

--- trunk/service/service2/gen/dict_USERS.cpp
+++ trunk/service/service2/gen/dict_USERS.cpp
@@ -8,9 +8,11 @@ dict(USER_AND_DEPT_NAME) {
 //DICT.outputl("xDICT=");
 //MV.outputl("xMV=");
 //ANS.outputl("xANS=");
+	return ANS;
 }
 
 dict(EXAMPLEDICTID2) {
 	ANS=RECORD(2)^"x";
+	return ANS;
 }
 
