e31f3de56cd28b5f676711daa1669fab2723e991
neosys.com <neosys.com@415b895b-543e-5d66-3bb8-6f700c98dabf>
2014-10-12 00:40:22 +0000

    natural sort works on key fields too now

--- trunk/service/service2/test/testnaturalsort.cpp
+++ trunk/service/service2/test/testnaturalsort.cpp
@@ -5,43 +5,67 @@ programinit()
 function main() {
 	printl("testnaturalsort says 'Hello World!'");
 
-	var tempsortfile="temptestnaturalsortfile";
-	deletefile(tempsortfile);
+	var tempfilename="tempsortfile";
+	tempfilename.outputl("tempfilename=");
+
+	var dicttempfilename="dict_"^tempfilename;
+	dicttempfilename.outputl("dicttempfilename=");
+
+	deletefile(tempfilename);
+	deletefile(dicttempfilename);
 
 	//let everything be one transaction
 	//with automatic rollback at close of program ^_^
 	begintrans();
 
-	createfile(tempsortfile);
-	clearfile(tempsortfile);
+	if (not createfile(tempfilename))
+		abort("cant create "^tempfilename);
+	if (not createfile(dicttempfilename))
+		abort("cant create "^dicttempfilename);
+	clearfile(tempfilename);
+	clearfile(dicttempfilename);
 
-	var ndigits=9;
-	var nzeros=25;
+	var tempfile;
+	if (not open(tempfilename,tempfile))
+		abort("cannot open "^tempfilename);
+
+	if (not write(
+	"F" _FM_
+	"0" _FM_
+	"ID" _FM_
+	_FM_
+	_FM_
+	_FM_
+	_FM_
+	_FM_
+	"R" _FM_
+	"20", dicttempfilename, "ID"))
+		abort("cannot write "^dicttempfilename^" ID");
+
+	//var ndigits=9;
+	//var nzeros=25;
+	var ndigits=3;
+	var nzeros=3;
 
 	//create records backwards to test sort
 	var ii,jj;
-	for (ii=ndigits;ii>=1;--ii)
+	for (ii=ndigits;ii>=0;--ii)
 		for (jj=nzeros;jj>=0;--jj) {
-			write("x",tempsortfile,"x"^ii^str("0",jj));
+			var key=""^ii^str("0",jj);
+			write("x",tempfile,key.outputl("key="));
 		}
 	//read sorted records forwards to check numbers
-	tempsortfile.select("by @ID");
-	for (jj=0;jj<=nzeros;++jj) {
-		for (ii=1;ii<=ndigits;++ii) {
-			var key;
-			var key2;
-			tempsortfile.readnext(key);
-			key2="x"^ii^str("0",jj);
-			if (key ne key2) {
-				printt(key,key2, "out of order");
-				printl();
-			}
-		}
+	if (not tempfile.select("by IDxxx"))
+		abort("problem sorting file");
+	var key;
+	//while (tempfile.readnext(key)) {
+	while (tempfile.readnext(key)) {
+		printl(key);
 	}
 
 	//rely on automatic rollback
-	//deletefile(tempsortfile);
-	//committrans();
+	//deletefile(tempfilename);
+	committrans();
 
 	return 0;
 }
