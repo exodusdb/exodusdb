acbbaa1217e41a18b66a00523f7a464712dd760d
neosys.com@gmail.com <neosys.com@gmail.com@415b895b-543e-5d66-3bb8-6f700c98dabf>
2014-12-24 18:01:51 +0000

    return empty string if givem empty string

--- trunk/service/service2/sys/quote2.cpp
+++ trunk/service/service2/sys/quote2.cpp
@@ -3,6 +3,8 @@ libraryinit()
 
 function main(in string0) {
 	//jbase linemark
+	if (not len(string0))
+		return string0;
 	var string1 = string0;
 	string1.converter(DQ, "\'");
 	string1.swapper(SVM, "\" \"");
