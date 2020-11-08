3cefc053f033df3028ab9b4eca8d9ca99c398467
NEOSYS Dev <dev@neosys.com>
2020-11-08 22:50:53 +0000

    mv singular.* from gen to sys

---
+++ service/src/sys/singular.cpp
@@ -0,0 +1,42 @@
+#include <exodus/library.h>
+libraryinit()
+
+function main(in pluralnoun) {
+	//c gen
+	var temp = pluralnoun;
+	var temp2 = pluralnoun;
+	temp2.ucaser();
+
+	if (temp2.substr(-2,2) == "ES") {
+
+		//COMPANIES=COMPANY
+		if (temp2.substr(-3,3) == "IES") {
+			temp.splicer(-3, 3, "Y");
+
+		//ADDRESSES=ADDRESS
+		} else if (temp2.substr(-4,4) == "SSES") {
+			temp.splicer(-2, 2, "");
+		} else if (temp2.substr(-4,4) == "SHES") {
+			temp.splicer(-2, 2, "");
+		} else if (temp2.substr(-4,4) == "CHES") {
+			temp.splicer(-2, 2, "");
+		} else {
+			temp.splicer(-1, 1, "");
+		}
+
+	//comment to help c++ decompiler
+	}else{
+
+		if (temp2[-1] == "S") {
+			//ANALYSIS, DOS
+			if ((temp2.substr(-2,2) ne "IS") and (temp2.substr(-2,2) ne "OS")) {
+				temp.splicer(-1, 1, "");
+			}
+		}
+
+	}
+
+	return temp;
+}
+
+libraryexit()
