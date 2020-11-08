b645d52a8676aaa47461aa61e0804a0658b5a42c
NEOSYS Dev <dev@neosys.com>
2020-11-08 20:13:35 +0000

    Revert "add various submodule files 2"
    This reverts commit 88fd376218d2fd6612f9cb13af3c9c8857d952f8.

--- service/src/job/prodinvs_common.h
+++
@@ -1,3 +0,0 @@
-//add any large new common variables to clearcommon routine in prodinvs
-
-	#define locklist win.registerx(5)
--- service/src/med/media_common.h
+++
@@ -1,5 +0,0 @@
-
-	programlinefns = "1" _VM_ "2" _VM_ "3" _VM_ "5" _VM_ "6" _VM_ "7" _VM_ "8" _VM_ "9" _VM_ "10" _VM_ "11" _VM_ "12" _VM_ "13" _VM_ "14" _VM_ "15" _VM_ "16" _VM_ "17" _VM_ "18" _VM_ "19" _VM_ "20" _VM_ "21" _VM_ "22" _VM_ "23" _VM_ "24" _VM_ "25" _VM_ "27" _VM_ "28";
-	nprogramlinefns = programlinefns.count(VM) + 1;
-	tvmonitorfns = "1" _VM_ "2" _VM_ "3" _VM_ "4" _VM_ "5" _VM_ "6" _VM_ "7" _VM_ "8" _VM_ "9" _VM_ "10" _VM_ "11" _VM_ "22" _VM_ "23" _VM_ "25" _VM_ "26" _VM_ "30";
-	ntvmonitorfns = tvmonitorfns.count(VM) + 1;
