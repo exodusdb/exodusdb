4544041f872b38dce877ce2c5bc16f560246235b
exodusdb <sb2@neosys.com>
2020-11-10 06:07:09 +0000

    add initcompany stub in ext to satisfy listen2

---
+++ service/src/ext/initcompany.cpp
@@ -0,0 +1,12 @@
+#include <exodus/library.h>
+libraryinit()
+
+//stub called from listen2 and maybe elsewhere
+//provide an alternative if desired
+
+function main(in nextcompanycode) {
+	false || nextcompanycode;
+	return 1;
+}
+
+libraryexit()
