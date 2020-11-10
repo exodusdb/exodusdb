e0bfca3fec8d55b23a6863305d98dbe78231b789
NEOSYS Dev <dev@neosys.com>
2020-11-10 20:25:57 +0000

    ext initcompany does basics

--- service/src/ext/initcompany.cpp
+++ service/src/ext/initcompany.cpp
@@ -1,11 +1,38 @@
 #include <exodus/library.h>
 libraryinit()
 
-//stub called from listen2 and maybe elsewhere
-//provide an alternative if desired
+#include <gen_common.h>
+
+var xx;
+var temp;
+var weeksperperiod;//num
+var ndec;//num
+var companydesc;
+var perioddesc;
+var currencydesc;
+var desc1;
+var desc2;
+var datax;
 
 function main(in nextcompanycode) {
-	false || nextcompanycode;
+    //c gen
+    #include <common.h>
+    #include <general_common.h>
+    //global desc1,desc2
+
+	gen.company = "";
+
+    if (not(nextcompanycode.unassigned())) {
+
+        if (nextcompanycode) {
+            if (not(xx.read(gen.companies, nextcompanycode))) {
+                //TODO return abort code and change all callers to handle failure
+                return 0;
+            }
+        }
+
+        gen.gcurrcompany = nextcompanycode;
+    }
 	return 1;
 }
 
