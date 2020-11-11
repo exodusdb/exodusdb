614990c82409bf0bfd27ee2aa26ef2778c9f3d26
NEOSYS Dev <dev@neosys.com>
2020-11-11 09:57:16 +0000

    add install_apt_www.sh

---
+++ install_apt_www.sh
@@ -0,0 +1,62 @@
+#!/bin/bash
+
+SECONDS=0
+
+echo Install apache and php
+echo ======================
+sudo apt install -y apache2 php php-xml
+#sudo git clone https://git.hosts.neosys.com/gits/neosys.net/ /var/www/exodus
+sudo a2enmod rewrite
+sudo systemctl restart apache2
+
+echo Configure an apache site - standard http
+echo ========================================
+cat > /tmp/exodus.conf <<V0G0N
+<VirtualHost *:80>
+        ServerAdmin webmaster@localhost
+
+        DocumentRoot /home/ubuntu/exodus/service/www
+
+        ErrorLog ${APACHE_LOG_DIR}/error.log
+        CustomLog ${APACHE_LOG_DIR}/access.log combined
+
+        <Directory /home/ubuntu/exodus/service/www>
+            Options Indexes FollowSymLinks
+            AllowOverride All
+            Require all granted
+            DirectoryIndex default.htm
+        </Directory>
+
+</VirtualHost>
+V0G0N
+sudo mv /tmp/exodus.conf /etc/apache2/sites-available/
+
+
+echo Enable the apache site and disable default
+echo ==========================================
+sudo a2dissite 000-default
+sudo a2ensite exodus
+sudo systemctl restart apache2
+
+
+echo Configure data permissions
+echo ==========================
+cd ~/exodus/service
+mkdir -p data
+chmod g+rws data
+mkdir -p data/exodus
+chmod o+rw data/exodus
+
+
+echo Compile the server2
+echo ===================
+echo ignore compile error on sys/server.cpp and various warnings
+cd ~/exodus/service/src
+. config
+cd sys
+../compall
+
+echo Finished
+echo ========
+duration=$SECONDS
+echo "$(($duration / 60)) minutes and $(($duration % 60)) seconds elapsed."
