7a71ac1f3b229060582b0893bdd67afa4a10269a
NEOSYS Dev <dev@neosys.com>
2020-11-11 10:44:13 +0000

    minor

--- install_www_apt.sh
+++ install_www_apt.sh
@@ -1,23 +1,20 @@
 #!/bin/bash
 
 set -x
-
-#
-# Installing exodus web service
-# ==========================
+:
+: Installing exodus web service
+: ==========================
 SECONDS=0
-
-#
-# Install apache and php
-# ======================
+:
+: Install apache and php
+: ======================
 sudo apt install -y apache2 php php-xml
-#sudo git clone https://git.hosts.neosys.com/gits/neosys.net/ /var/www/exodus
+:sudo git clone https://git.hosts.neosys.com/gits/neosys.net/ /var/www/exodus
 sudo a2enmod rewrite
 sudo systemctl restart apache2
-
-#
-# Configure an apache site - standard http
-# ========================================
+:
+: Configure an apache site - standard http
+: ========================================
 cat > /tmp/exodus.conf <<V0G0N
 <VirtualHost *:80>
         ServerAdmin webmaster@localhost
@@ -37,34 +34,30 @@ cat > /tmp/exodus.conf <<V0G0N
 </VirtualHost>
 V0G0N
 sudo mv /tmp/exodus.conf /etc/apache2/sites-available/
-
-#
-# Enable the apache site and disable default
-# ==========================================
+:
+: Enable the apache site and disable default
+: ==========================================
 sudo a2dissite 000-default
 sudo a2ensite exodus
 sudo systemctl restart apache2
-
-#
-# Configure data permissions
-# ==========================
+:
+: Configure data permissions
+: ==========================
 cd ~/exodus/service
 mkdir -p data
 chmod g+rws data
 mkdir -p data/exodus
 chmod o+rw data/exodus
-
-#
-# Compile the service
-# ===================
-# ignore compile error on sys/server.cpp and various warnings
+:
+: Compile the service
+: ===================
+: ignore compile error on sys/server.cpp and various warnings
 cd ~/exodus/service/src
 . config
 cd sys
 ../compall
-
-#
-# Finished - Exodus is listening on port 80
-# =========================================
+:
+: Finished - Exodus is listening on port 80
+: =========================================
 duration=$SECONDS
-# "$(($duration / 60)) minutes and $(($duration % 60)) seconds elapsed."
+: "$(($duration / 60)) minutes and $(($duration % 60)) seconds elapsed."
