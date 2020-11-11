77b005b9015aaff8ede3d2fac06561512df6aeb5
NEOSYS Dev <dev@neosys.com>
2020-11-11 13:22:49 +0000

    add ext changelogsubs

---
+++ service/src/ext/changelogsubs.cpp
@@ -0,0 +1,21 @@
+#include <exodus/library.h>
+libraryinit()
+
+function main(in mode0) {
+
+    if (mode0.a(1) == "SELECTANDLIST") {
+
+    } else if (mode0.a(1) == "WHATSNEW") {
+
+    } else if (mode0 == "GETVERSIONDATES") {
+
+    } else if (mode0.a(1) == "SELECT") {
+
+    } else if (mode0.a(1) == "LIST") {
+
+    }
+
+    return 0;
+}
+
+libraryexit()
