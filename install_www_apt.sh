945b04e0d07f76f593f3bca8312a0ed953f8b65e
NEOSYS Dev <dev@neosys.com>
2020-11-11 10:31:09 +0000

    minor

--- install_www_apt.sh
+++ install_www_apt.sh
@@ -1,22 +1,23 @@
 #!/bin/bash
 
+set -x
+
+#
+# Installing exodus web service
+# ==========================
 SECONDS=0
 
-echo
-echo Installing exodus web service
-echo ==========================
-
-echo
-echo Install apache and php
-echo ======================
+#
+# Install apache and php
+# ======================
 sudo apt install -y apache2 php php-xml
 #sudo git clone https://git.hosts.neosys.com/gits/neosys.net/ /var/www/exodus
 sudo a2enmod rewrite
 sudo systemctl restart apache2
 
-echo
-echo Configure an apache site - standard http
-echo ========================================
+#
+# Configure an apache site - standard http
+# ========================================
 cat > /tmp/exodus.conf <<V0G0N
 <VirtualHost *:80>
         ServerAdmin webmaster@localhost
@@ -37,33 +38,33 @@ cat > /tmp/exodus.conf <<V0G0N
 V0G0N
 sudo mv /tmp/exodus.conf /etc/apache2/sites-available/
 
-echo
-echo Enable the apache site and disable default
-echo ==========================================
+#
+# Enable the apache site and disable default
+# ==========================================
 sudo a2dissite 000-default
 sudo a2ensite exodus
 sudo systemctl restart apache2
 
-echo
-echo Configure data permissions
-echo ==========================
+#
+# Configure data permissions
+# ==========================
 cd ~/exodus/service
 mkdir -p data
 chmod g+rws data
 mkdir -p data/exodus
 chmod o+rw data/exodus
 
-echo
-echo Compile the service
-echo ===================
-echo ignore compile error on sys/server.cpp and various warnings
+#
+# Compile the service
+# ===================
+# ignore compile error on sys/server.cpp and various warnings
 cd ~/exodus/service/src
 . config
 cd sys
 ../compall
 
-echo
-echo Finished - Exodus is listening on port 80
-echo =========================================
+#
+# Finished - Exodus is listening on port 80
+# =========================================
 duration=$SECONDS
-echo "$(($duration / 60)) minutes and $(($duration % 60)) seconds elapsed."
+# "$(($duration / 60)) minutes and $(($duration % 60)) seconds elapsed."
