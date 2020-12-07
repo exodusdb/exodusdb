423727615168a78883e96e8cebb7839b9f5fafe5
NEOSYS Dev <dev@neosys.com>
2020-12-07 21:38:22 +0000

    convpdf add to enable local file access on linux)

--- service/src/sys/convpdf.cpp
+++ service/src/sys/convpdf.cpp
@@ -12,12 +12,25 @@ function main(in printopts0, out errors) {
 
 	//gets filename from system<2> and updates system<2> if successful
 
-		/* install for ubuntu 18.04;
+		/*install for Ubuntu 18.04;
+
+		//doesnt work well
+		sudo apt-get install wkhtmltopdf;
+
+		//or
+
+		https://wkhtmltopdf.org/downloads.html;
+		wget https://github.com/wkhtmltopdf/packaging/releases/download/0.12.6-1/wkhtmltox_0.12.6-1.bionic_amd64.deb;
+		dpkg -i wkhtmltox_0.12.6-1.bionic_amd64.deb;
+
 		https://wkhtmltopdf.org/;
 		wget https://downloads.wkhtmltopdf.org/0.12/0.12.5/wkhtmltox_0.12.5-1.bionic_amd64.deb;
 		dpkg -i wkhtmltox_0.12.5-1.bionic_amd64.deb;
 		apt --fix-broken install;
-		wkhtmltopdf http://google.com google.pdf;
+
+		//test
+		/usr/local/bin/wkhtmltopdf http://google.com google.pdf;
+
 		*/
 
 	var htmlfilename = SYSTEM.a(2);
@@ -48,6 +61,9 @@ function main(in printopts0, out errors) {
 		}
 	}else{
 		pdfcmd = "/usr/local/bin/wkhtmltopdf";
+		if (not(pdfcmd.osfile())) {
+			pdfcmd = "/usr/bin/wkhtmltopdf";
+		}
 	}
 
 	//quit without conversion if conversion program doesnt exist
@@ -70,6 +86,8 @@ function main(in printopts0, out errors) {
 	}
 
 	pdfcmd ^= " --print-media-type";
+	//required on 18.04 to access local images
+	pdfcmd ^= " --enable-local-file-access";
 
 	pdfcmd ^= " -q " ^ htmlfilename ^ " " ^ pdffilename;
 
@@ -78,6 +96,8 @@ function main(in printopts0, out errors) {
 
 	if (errors) {
 		errors = "html2pdf: " ^ errors;
+		printl(pdfcmd);
+		printl(errors);
 	}else{
 		SYSTEM.r(2, pdffilename);
 	}
