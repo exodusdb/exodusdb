77ecc80c2c2d8abea9edd4264ef09728cb6fbdc4
root <sb2@neosys.com>
2021-04-17 16:23:28 +0000

    add comment how to get fixed version of fast_float in case it make breaking changes again

--- get_fast_float.sh
+++ get_fast_float.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 test -d fast_float || git clone https://github.com/fastfloat/fast_float.git
 cd fast_float/
+#this version worked after skipping leading plus signs eg "+.99" isnum but not for fast_float
+#git reset --hard 6c97156f6de7a40119a2f1a4913a457e69d34b12
 cmake .
 make
 make install
