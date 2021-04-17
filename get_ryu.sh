62b98755e84fc6ef65ff4f9acea21d986e47869a
root <sb2@neosys.com>
2021-04-17 14:32:03 +0000

    fix installation of bazel build system in get_ryu

--- get_ryu.sh
+++ get_ryu.sh
@@ -5,10 +5,11 @@ set -eux
 : ==========================
 :
 	#https://docs.bazel.build/versions/master/install-ubuntu.html
-	sudo apt install apt-transport-https curl gnupg
+	sudo apt install -y apt-transport-https curl gnupg
 	curl -fsSL https://bazel.build/bazel-release.pub.gpg | gpg --dearmor > bazel.gpg
 	sudo mv bazel.gpg /etc/apt/trusted.gpg.d/
 	echo "deb [arch=amd64] https://storage.googleapis.com/bazel-apt stable jdk1.8" | sudo tee /etc/apt/sources.list.d/bazel.list
+	sudo apt update && sudo apt -y install bazel
 
 : get ryu
 : =======
