41a82c2c5a39ee93754fc17966a7fff23620ce9c
root <sb2@neosys.com>
2021-08-18 12:16:13 +0000

    sendmail deflects if /hosts/serve_agy.disabled present. test_zzclean doesnt run if no database

--- test/src/test_zzzclean.cpp
+++ test/src/test_zzzclean.cpp
@@ -2,6 +2,14 @@
 programinit()
 
 function main() {
+
+	//Pass if no default database connection
+	if (not connect()) {
+		printl("No default db connection to perform db testing.");
+		printl("Test passed");
+		return 0;
+	}
+
 	var filenames = listfiles();
 	for (var filename : filenames) {
 		if (filename.index("xo_"))
