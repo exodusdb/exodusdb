bb4b2b5f350ef233be13cdc82c11e8e8e70d1ca9
root <sb2@neosys.com>
2022-01-16 16:15:07 +0000

    add fast_float as a submodule since it still makes ascii to float conversion much faster than from_chars or strtod  in g++ v11

--- exodus/libexodus/exodus/mvfuncs.cpp
+++ exodus/libexodus/exodus/mvfuncs.cpp
@@ -55,23 +55,19 @@ Binary    Hex          Comments
 // I would have used it if it were available back then!"
 //
 //double only available in gcc 11 onwards. msvc has it from 2017 or 19
-#if __GNUC__ >= 11
-#define USE_CHARCONV
-#define STD_OR_FASTFLOAT std
-#else
-
 //https://github.com/fastfloat/fast_float
 #if __has_include(<fast_float/fast_float.h>)
 #define HAS_FASTFLOAT
 #include <fast_float/fast_float.h>
 #define STD_OR_FASTFLOAT fast_float
-#endif
 
-#if __has_include(<ryu/ryu.h>)
+#elif __GNUC__ >= 11
+#define USE_CHARCONV
+#define STD_OR_FASTFLOAT std
+
+#elif __has_include(<ryu/ryu.h>)
 #define HAS_RYU
 #include <ryu/ryu.h>
-#endif
-
 #endif
 
 //gcc 10 doesnt include conv from and to floating point
@@ -1923,16 +1919,6 @@ bool var::isnum(void) const {
 			auto [p, ec] = STD_OR_FASTFLOAT::from_chars(first, var_str.data() + var_str.size(), var_dbl, STD_OR_FASTFLOAT::chars_format::fixed);
 			if (ec != std::errc()) {
 				//std::cerr << "parsing failure\n"; return EXIT_FAILURE;
-				var_typ = VARTYP_NANSTR;
-				return false;
-			}
-
-#elif defined(USE_CHARCONV)
-
-			//std::cout << "from_chars decimal iconv" << var_str << std::endl;
-
-			auto [p, ec] = std::from_chars(var_str.data(), var_str.data() + var_str.size(), var_dbl);
-			if (ec != std::errc()) {
 				var_typ = VARTYP_NANSTR;
 				return false;
 			}
