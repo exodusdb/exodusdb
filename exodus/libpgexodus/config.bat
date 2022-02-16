00da31c2766b7e5e520638bee78249837664fac3
root <sb2@neosys.com>
2022-02-16 05:32:40 +0000

    The Big Cleanup continued. Remove all automake files and apt packaging

--- exodus/libpgexodus/config.bat
+++
@@ -1,4 +0,0 @@
-@rem config for installation
-set POSTGRES_SERVICE=postgresql-8.3
-set POSTGRES_PATH=\program files\postgresql\8.3
-set RELEASE_OR_DEBUG=release
--- exodus/libpgexodus/install.bat
+++
@@ -1,4 +0,0 @@
-call config.bat
-net stop "%POSTGRES_SERVICE%"
-net start "%POSTGRES_SERVICE%"
-copy %RELEASE_OR_DEBUG%\pgexodus.dll "%POSTGRES_PATH%\lib"
