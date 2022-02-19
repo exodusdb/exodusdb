8a5325035e7229dfcdb985e65fab99d8c2ed3477
root <sb2@neosys.com>
2022-02-19 10:23:50 +0000

    slight improvements in test_perf

--- test/src/test_perf.cpp
+++ test/src/test_perf.cpp
@@ -19,86 +19,64 @@ function main() {
     int nn = 1000000;
     var setup_time;
 
-	printl(R"V0G0N(
-============================================
-WAS THE FOLLOWING ON UBUNTU 20.04
-Intel(R) Xeon(R) CPU E5620  @ 2.40GHz (2010)
-9 ns construct unassigned var in int loop
+	bool the_truth_hurts = false;
+	double some_dbl = 1.1;
+	char some_char = ' ';
 
-49 ns construct+destruct var from const char* '12345.67' in int loop
-69 ns convert var string '12345.67' to double
-
-9 ns construct+destruct var from double 12345.67 in int loop
-529 ns convert var double 12345.67 to string
-============================================
-)V0G0N");
-
-	printl(R"V0G0N(
-============================================
-WAS THE FOLLOWING ON UBUNTU 22.04
-Intel(R) Xeon(R) CPU E5620  @ 2.40GHz (2010)
-9 ns construct unassigned var in int loop
-
-20 ns construct+destruct var from const char* '12345.67' in int loop
-58 ns convert var string '12345.67' to double
-
-8 ns construct+destruct var from double 12345.67 in int loop
-215 ns convert var double 12345.67 to string
-============================================
-)V0G0N");
-
-    var test = "Time creation+destruction of unassigned var within int loop";
+    printl("Exp: 7 ns Time creation+destruction of unassigned var + test within int loop");
     {
         var started = ostime();
         for (int ii=0;ii < nn; ii++) {
-           var();
+           the_truth_hurts = var().assigned();
         }
         setup_time = ostime() - started;
-        printl(round((setup_time) / nn * 1E9), "ns", test);
+        printl("Act:", round((setup_time) / nn * 1E9), "ns");
     }
 
     printl();
 
-    test = "Time creation+destruction of string var '12345.67' within int loop";
+    printl("Exp: 15  ns - Creation+destruction of string var '12345.67' + test within int loop");
     {
         var started = ostime();
         for (int ii=0;ii < nn; ii++) {
-           var("12345.67");
+           the_truth_hurts = var("12345.67").assigned();
         }
         setup_time = ostime() - started;
-        printl(round((setup_time) / nn * 1E9), "ns", test);
+        printl("Act:", round((setup_time) / nn * 1E9), "ns");
     }
+	printl();
 
-    test = "Time conversion of string var '12345.67' to double (Using fast_float::from_chars on Ubuntu 20.04/g++v9.3 and Ubuntu 22.04/g++v11.2)";
+	printl("Exp: 65 ns - Conversion of string var '12345.67' to double (Using fast_float::from_chars on Ubuntu 20.04/g++v9.3 and Ubuntu 22.04/g++v11.2)");
     {
         var started = ostime();
         for (int ii=0;ii < nn; ii++) {
-           //var("12345.67").toDouble();
-           var("12345.67").isnum();
+           some_dbl = var("12345.67").toDouble();
+           //var("12345.67");
         }
-        printl(round((ostime() - started - setup_time) / nn * 1E9), "ns", test);
+        printl("Act:", round((ostime() - started - setup_time) / nn * 1E9), "ns");
     }
 
     printl();
 
-    test = "Time creation+destruction of double var 12345.67 within int loop";
+    printl("Exp: 6 ns - Creation+destruction of double var 12345.67 + test within int loop");
     {
         var started = ostime();
         for (int ii=0;ii < nn; ii++) {
-           var(12345.67);
+           the_truth_hurts = var(12367).assigned();
         }
         setup_time = ostime() - started;
-        printl(round((setup_time) / nn * 1E9), "ns", test);
+        printl("Act:", round((setup_time) / nn * 1E9), "ns");
     }
+	printl();
 
-    test = "Time conversion of double var 12345.67 to string and return length (Using RYU D2S on Ubuntu 20.04/g++v9.3 and std::to_chars on Ubuntu 22.04/g++v11.2)";
+    printl("Exp: 650  ns - Conversion of double var 12345.67 to string and return length (Using RYU D2S on Ubuntu 20.04/g++v9.3 and std::to_chars on Ubuntu 22.04/g++v11.2)");
     {
-		int dummy;
+		var nn = 10'000'000;
         var started = ostime();
         for (int ii=0;ii < nn; ii++) {
-           dummy = var(12345.67).length();
+           some_char = var(12345.67).toChar();
         }
-        printl(round((ostime() - started - setup_time) / nn * 1E9), "ns", test, dummy);
+        printl("Act:", round((ostime() - started - setup_time) / nn * 1E9), "ns");
     }
 
 
@@ -111,131 +89,143 @@ Intel(R) Xeon(R) CPU E5620  @ 2.40GHz (2010)
 		int firstcasen = COMMAND.a(2);
 		if (not firstcasen)
 			firstcasen = 0;
-		int ncases = 19;
+
+		int ncases = 20;
+
 		for (int casen = firstcasen; casen <= ncases; casen++) {
 
-			int nn = 100'000'000;
-
-			int i1 = 0;
-//			double d1 = 0;
-//			char c1 = '\0';
-//			char* cp1 = nullptr;
-			bool b1;
-			var v1 = 0;
-			std::string ss1 = "x";
-
 			printl();
-			var started = ostime();
+			for (int repeatn = 0; repeatn < 3; repeatn++) {
 
-			switch (casen) {
+				int nn = 100'000'000;
 
-			break; case 0:
-				printl("Exp: 45     ns - old var method - for (var;;)");
-				nn = 10'000'000;
-				for (var v2 = 0; v2 <= nn; v2++)
-					{i1 = v2;};
+				int i1 = 1;
+	//			double d1 = 0;
+	//			char c1 = '\0';
+	//			char* cp1 = nullptr;
+				bool b1 = true;
+				var v1 = 1;
+				std::string ss1 = "x";
 
-			break; case 1:
-				printl("Exp: 20     ns - new var method - for (var:range)");
-				for (const var v2 : range(0 to nn))
-					{i1 = v2;};
+				var started = ostime();
 
-			break; case 2:
-				printl("Exp: 0.840 ns - old int method - for (int;;)");
-				for (int i2 = 0; i2 <= nn; i2++)
-					{i1 = i2;};
+				switch (casen) {
 
-			break; case 3:
-				printl("Exp: 0.840 ns - new int method - for (int:range)");
-				for (int i2 : range(0 to nn))
-					{i1 = i2;};
+				break; case 0:
+					if (repeatn == 0) printl("Exp: 35     ns - old var method - for (var;;)");
+					nn = 10'000'000;
+					for (var v2 = 0; v2 <= nn; v2++)
+						{i1 = v2;};
 
-			break; case 4:
-				printl("Exp: 0.840 ns - new int method using boost - for (int:range)");
-				for (int i2 : boost::irange(0, nn))
-					{i1 = i2;};
+				break; case 1:
+					if (repeatn == 0) printl("Exp: 15     ns - new var method - for (var:range)");
+					for (const var v2 : range(0 to nn))
+						{i1 = v2;};
 
-			break; case 5:
-				printl("Exp: 5.5   ns - empty var + test");
-				for (int i2 = 0; i2 <= nn; i2++)
-					{var x; b1 = x.unassigned();};
+				break; case 2:
+					if (repeatn == 0) printl("Exp: 0.840 ns - old int method - for (int;;)");
+					for (int i2 = 0; i2 <= nn; i2++)
+						{i1 = i2;};
 
-			break; case 6:
-				printl("Exp: 6.5   ns - construct from int + test");
-				for (int i2 = 0; i2 <= nn; i2++)
-					{var i = 123456; b1 = i.assigned();};
+				break; case 3:
+					if (repeatn == 0) printl("Exp: 0.840 ns - new int method - for (int:range)");
+					for (int i2 : range(0 to nn))
+						{i1 = i2;};
 
-			break; case 7:
-				printl("Exp: 6.5   ns - construct from bool + test");
-				for (int i2 = 0; i2 <= nn; i2++)
-					{var b = true; b1 = b.assigned();};
+				break; case 4:
+					if (repeatn == 0) printl("Exp: 0.840 ns - new int method using boost - for (int:range)");
+					for (int i2 : boost::irange(0, nn))
+						{i1 = i2;};
 
-			break; case 8:
-				printl("Exp: 6.5   ns - construct from double + test");
-				for (int i2 = 0; i2 <= nn; i2++)
-					{var d = 123.456; b1 = d.assigned();};
+				break; case 5:
+					if (repeatn == 0) printl("Exp: 6.5   ns - construct empty var + test");
+					for (int i2 = 0; i2 <= nn; i2++)
+						{var x; b1 = x.assigned();};
 
-			break; case 9:
-				printl("Exp: 11    ns - construct from cstr + test");
-				for (int i2 = 0; i2 <= nn; i2++)
-					{var s = "1"; b1 = s.assigned();};
+				break; case 6:
+					if (repeatn == 0) printl("Exp: 6.5   ns - construct from int + test");
+					for (int i2 = 0; i2 <= nn; i2++)
+						{var i = 123456; b1 = i.assigned();};
 
-			break; case 10:
-				printl("Exp: 11    ns - construct from char + test");
-				for (int i2 = 0; i2 <= nn; i2++)
-					{var s = '1'; b1 = s.assigned();};
+				break; case 7:
+					if (repeatn == 0) printl("Exp: 6.5   ns - construct from bool + test");
+					for (int i2 = 0; i2 <= nn; i2++)
+						{var b = true; b1 = b.assigned();};
 
-			break; case 11:
-				printl("Exp: 11    ns - construct from std::string + test");
-				for (int i2 = 0; i2 <= nn; i2++)
-					{var s = ss1; b1 = s.assigned();};
+				break; case 8:
+					if (repeatn == 0) printl("Exp: 6.5   ns - construct from double + test");
+					for (int i2 = 0; i2 <= nn; i2++)
+						{var d = 123.456; b1 = d.assigned();};
 
-			break; case 12:
-				printl("Exp: 4     ns - pure test");
-				for (int i2 = 0; i2 <= nn; i2++)
-					{b1 = v1.assigned();};
+				break; case 9:
+					if (repeatn == 0) printl("Exp: 12    ns - construct from cstr + test");
+					for (int i2 = 0; i2 <= nn; i2++)
+						{var s = "1"; b1 = s.assigned();};
 
-			break; case 13:
-				printl("Exp: 0.84  ns - assign from int");
-				for (int i2 = 0; i2 <= nn; i2++)
-					{v1 = 123456;};
+				break; case 10:
+					if (repeatn == 0) printl("Exp: 12    ns - construct from char + test");
+					for (int i2 = 0; i2 <= nn; i2++)
+						{var s = '1'; b1 = s.assigned();};
 
-			break; case 14:
-				printl("Exp: 1     ns - assign from bool");
-				for (int i2 = 0; i2 <= nn; i2++)
-					{v1 = true;};
+				break; case 11:
+					if (repeatn == 0) printl("Exp: 12    ns - construct from std::string + test");
+					for (int i2 = 0; i2 <= nn; i2++)
+						{var s = ss1; b1 = s.assigned();};
 
-			break; case 15:
-				printl("Exp: 1     ns - assign from double");
-				for (int i2 = 0; i2 <= nn; i2++)
-					{v1 = 123.456;;};
+				break; case 12:
+					if (repeatn == 0) printl("Exp: 4.5   ns - pure test");
+					for (int i2 = 0; i2 <= nn; i2++)
+						{b1 = v1.assigned();};
 
-			break; case 16:
-				printl("Exp: 11    ns - assign from cstr");
-				for (int i2 = 0; i2 <= nn; i2++)
-					{v1 = "1";};
+				break; case 13:
+					if (repeatn == 0) printl("Exp: 0.9   ns - assign from int");
+					for (int i2 = 0; i2 <= nn; i2++)
+						{v1 = 123456;};
 
-			break; case 17:
-				printl("Exp: 11    ns - assign from char");
-				for (int i2 = 0; i2 <= nn; i2++)
-					{v1 = '1';};
+				break; case 14:
+					if (repeatn == 0) printl("Exp: 0.9   ns - assign from bool");
+					for (int i2 = 0; i2 <= nn; i2++)
+						{v1 = true;};
 
-			break; case 18:
-				printl("Exp: 11    ns - assign from std::string");
-				for (int i2 = 0; i2 <= nn; i2++)
-					{v1 = ss1;};
+				break; case 15:
+					if (repeatn == 0) printl("Exp: 1.3   ns - assign from double");
+					for (int i2 = 0; i2 <= nn; i2++)
+						{v1 = 123.456;};
 
-			break; case 19:
-				printl("Exp: 11    ns - var + int + test");
-				for (int i2 = 0; i2 <= nn; i2++)
-					{b1 = (v1 + i2).assigned();};
+				break; case 16:
+					if (repeatn == 0) printl("Exp: 13    ns - assign from cstr");
+					for (int i2 = 0; i2 <= nn; i2++)
+						{v1 = "1";};
 
+				break; case 17:
+					if (repeatn == 0) printl("Exp: 11    ns - assign from char");
+					for (int i2 = 0; i2 <= nn; i2++)
+						{v1 = '1';};
+
+				break; case 18:
+					if (repeatn == 0) printl("Exp: 11    ns - assign from std::string");
+					for (int i2 = 0; i2 <= nn; i2++)
+						{v1 = ss1;};
+
+				break; case 19:
+					if (repeatn == 0) printl("Exp: 22    ns - var + int + test");
+					for (int i2 = 0; i2 <= nn; i2++)
+						{b1 = (v1 + i2).assigned();};
+
+				break; case 20:
+					if (repeatn == 0) printl("Exp: 22    ns - var + 0.1 + test");
+					for (int i2 = 0; i2 <= nn; i2++)
+						{b1 = (v1 + 0.1).assigned();};
+
+				}
+				var ended = ostime();
+				if (i1 or b1 or v1)
+					printl("Act:", ((ended - started) / int(nn) * 1e9).round(3), "ns");
 			}
-			var ended = ostime();
-			if (i1 or b1 or true)
-				printl("Act:", ((ended - started) / int(nn) * 1e9).round(3), "ns");
 		}
 	}
+
+	printl(the_truth_hurts, some_dbl, some_char);
 
     printl();
 	printl("Test passed");
