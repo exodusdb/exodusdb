986b1858922e495008222eb5675e4b5f5bc2994e
root <sb2@neosys.com>
2022-03-25 08:44:29 +0000

    fix list to handle quotes and punctuation characters correctly in interactive/TERMINAL mode (reconstruct original command before shelling out to support paging)

--- cli/src/list.cpp
+++ cli/src/list.cpp
@@ -234,8 +234,41 @@ function main() {
 	showborder = 0;
 	headtabperpage = 1;
 
-	if (TERMINAL) {
-		return osshell("TERM= " ^ COMMAND.convert(FM, " ") ^ " | pager --chop-long-lines --quit-if-one-screen");
+	//TRACE(SENTENCE)
+	//TRACE(COMMAND)
+
+	if (TERMINAL and not OPTIONS.index("C")) {
+		// Switch off TERMINAL
+		var oscmd = "TERM=";
+
+		// Reconstruct the list command into a format suitable for processing by bash
+		// If word has any bash special characters or spaces
+		// then escape any single quotes and wrap in single quotes
+		for (var word : COMMAND) {
+
+			var word2 = word.convert(R"( `~!@#$%^&*(){}[]:;'"|<>?\)" to "");
+			if (word2.len() < word.len()) {
+
+				// Single quotes inside single quotes can be represented as '"'"' for bash
+				// The leading and trailng aingle quotes in '"'"' terminate and resume the single quoted string
+				// so 'abc'"'"'def' is actually abc'def
+				word.swapper("'", R"('"'"')");
+
+				word.squoter();
+			}
+
+			oscmd ^= " " ^ word;
+		}
+
+		// Add any options
+		if (OPTIONS)
+			oscmd ^= " {"^ OPTIONS ^ "}";
+
+		// Pipe into pager
+		oscmd ^= " | pager --chop-long-lines --quit-if-one-screen";
+
+		//TRACE(oscmd)
+		return osshell(oscmd);
 	}
 
 //	if (USERNAME eq "EXODUS") {
@@ -587,7 +620,6 @@ nextkey:
 	} else if ((word.substr(1, 4) eq "LIST" or word.substr(1, 5) eq "NLIST") or word eq "XLIST") {
 		ss ^= "SELECT";
 		goto filename;
-		{}
 
 	} else if (word eq "GETLIST") {
 		gosub getword();
@@ -692,7 +724,6 @@ nextkey:
 					}
 					limits(3, nlimits, -1) = word;
 				}
-				{}
 			}  //loop;
 		}
 
@@ -1981,7 +2012,6 @@ subroutine getquotedword2() {
 }
 
 subroutine getword() {
-	{}
 
 getnextword:
 	////////////
@@ -2009,12 +2039,12 @@ getnextword:
 	}
 
 	//!** call note(quote(word):' ':quote(nextword))
-
+	//TRACE(word)
+	//TRACE(nextword)
 	return;
 }
 
 subroutine getword2() {
-	{}
 
 getword2b:
 	//////////
