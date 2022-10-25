7536d4c4f3ba34305413e9a811b697c2d6a6c9ce
root <sb2@neosys.com>
2022-10-25 06:05:13 +0000

    Improvements to install_apt.sh. Defaults to build type RelWithDebInfo (i.e. -O2 -g). Tests are run before installing. Target dir ~/exodus can be customised using $1 or $EXODUSDIR.

--- install_apt.sh
+++ install_apt.sh
@@ -12,11 +12,13 @@ set -euxo pipefail
 : or
 : "curl https://raw.githubusercontent.com/exodusdb/exodusdb/master/install_apt.sh | less"
 : "curl https://raw.githubusercontent.com/exodusdb/exodusdb/master/install_apt.sh | bash"
+
 :
 : Installing on:
 :
 	uname -a
 	cat /etc/issue
+
 :
 : Tested on:
 :
@@ -30,14 +32,21 @@ set -euxo pipefail
 : "Linux lucid64 2.6.32-31-server" #61-Ubuntu SMP Fri Apr 8 19:44:42 UTC 2011 x86_64 GNU/Linux
 : "Debian GNU/Linux 6.0 \n \l"
 : "Linux debian32 2.6.32-5-686" #1 SMP Wed May 18 07:08:50 UTC 2011 i686 GNU/Linux
-:
+
 :
 : 1. Building and Installing Exodus
 : =================================
 :
+:
+: 'Install into $1, $EXODUSDIR or ~/exodus'
+:
+	EXODUSDIR=${1:-${EXODUSDIR:-~/exodus}}
+
+:
 : Update repos in case building on a pristine installation
 :
 	sudo apt-get update
+
 :
 : Install dependencies
 :
@@ -53,22 +62,26 @@ set -euxo pipefail
 		#
 		# was:
 		#libboost-dev libboost-system-dev libboost-regex-dev libboost-thread-dev libboost-locale-dev libboost-chrono-dev libboost-date-time-dev libboost-atomic-dev
+
 :
 : Download exodus if not already present
 :
 	cd ~
 	[ ! -d exodus ] && git clone https://github.com/exodusdb/exodusdb exodus
+
 :
 : Refresh git in case reinstalling
 :
-	cd ~/exodus
+	cd $EXODUSDIR
 	git stash
 	git pull
+
 :
-: Config make with all cpus in parallel
+: Config make with all CPUs in parallel
 :
-	cd ~/exodus
-	cmake .
+	cd $EXODUSDIR
+	cmake . -D CMAKE_BUILD_TYPE=${BUILD_TYPE:-RelWithDebInfo}
+
 :
 : Clean all - unless suppressed for speed. Required in case half-built with wrong libs somehow.
 :
@@ -76,11 +89,17 @@ set -euxo pipefail
 	cmake .
 
 :
-: Make all exodus lib, cli and pgexodus
+: Make all exodus lib, cli and pgexodus with all CPUs in parallel. Hopefully enough memory.
 :
 	make -j `nproc`
+
 :
-: Install all exodus lib and cli
+: Skip full testing unless FORCE_TEST is set
+:
+	${FORCE_TEST:-} && CTEST_OUTPUT_ON_FAILURE=1 CTEST_PARALLEL_LEVEL=`nproc` make test
+
+:
+: Install all exodus lib and cli - Probably into /usr/local ... lib, bin, include/exodus and share/exodus
 :
 	sudo make install
 
@@ -93,6 +112,7 @@ set -euxo pipefail
         #yum -y install postgresql-server
         sudo DEBIAN_FRONTEND=noninteractive \
         apt-get -y install postgresql postgresql-client
+
 :
 : Restart postgres
 :
@@ -103,7 +123,7 @@ set -euxo pipefail
 :
 : make install again to install pgexodus extension and functions, and exodus user and database
 :
-        cd ~/exodus
+        cd $EXODUSDIR
         make install
 
 :
@@ -118,28 +138,33 @@ set -euxo pipefail
 	user=exodus \
 	password=somesillysecret \
 	> ~/.config/exodus/exodus.cfg
+
 :
 : 4. Add some postgres utility functions
 : ======================================
 :
-: one of the exodus cli programs
+: Use an exodus cli program
 :
 	dict2sql
+
 :
 : 5. Testing Exodus
 : =================
+:
 : Every database requires a lists file in order to store select lists
 :
 	createfile lists || true
 	cd ~
 	testsort
+
 :
 : 6. Programming with Exodus
 : ==========================
-: you must make some change to hello or actually save it, not just quit
+: You must make some change to hello or actually save it, not just quit.
 : edic hello
 : hello
 : compile hello
+
 :
 : 7. Finished $0 $* in $((SECONDS / 60)) minutes and $((SECONDS % 60)) seconds
 : ======================================================================
