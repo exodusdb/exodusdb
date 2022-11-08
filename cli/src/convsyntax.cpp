50ce53c8aee1ea9c5568fdc01b7a7df023efe5d4
root <sb2@neosys.com>
2022-11-08 11:34:58 +0000

    + SB: cli convsyntax add ff option to convert "for" loops without var declaration to range based syntax

--- cli/src/convsyntax.cpp
+++ cli/src/convsyntax.cpp
@@ -9,7 +9,7 @@ SYNTAX
 EXAMPLE
 	conv_syntax ~/neosys/src/ASTERISK/ASTERISK.cpp {RF}
 OPTIONS
-	R - Change replacer syntax
+	r - Change replacer syntax
 		   x.r(a,b,c,y)
 		-> x(a,b,c) = y
 		Examples:
@@ -19,44 +19,46 @@ OPTIONS
 		-> x(fn, vn) = replacement;
 		   x.r(fn, vn, sn, replacement);
 		-> x(fn, vn, sn) = replacement;
-	F - Change for loop syntax to use range()
-		   for (const var x = ...
-		-> for (var x : range(...
+	f - Change for loop syntax to use range (1)
 		Example:
-		for (const var x = a; x <= b; ++x)
+		   for (var x = a; x <= b; ++x)
 		-> for (var x : range(a to b))
-	B - Change substr to b
+   ff - Change for loop syntax to use range (2)
+		Example:
+		   for (x = a; x <= b; ++x)
+		-> for (x : range(a to b))
+	b - Change substr to b
 		   .substr(
 		-> .b(
-	S - Change .b(1, n) eq/== to .starts(
+	s - Change .b(1, n) eq/== to .starts(
 	    and    '.b(n, n)' to .first(n)
 		   .b(1, n) eq "XXX"
 		-> .starts("XXX")
 		   .b(3, 3)
 		-> .first(3)
-	L - Change '.b(-n, n) eq' to .ends(n)
+	l - Change '.b(-n, n) eq' to .ends(n)
 	    and    '.b(-n, n)' to .last(n)
 		   .b(1, n) eq "XXX"
 		-> .starts("XXX")
 		   .b(-3, 3)
 		-> .last(3)
-	C - Cut
+	c - Cut
 		   .b(2)
 		-> .cut(1)
 		...
 		   .b(10)
 		-> .cut(9)
-	P - paster
+	p - paster
 		   .paster(1, n, ""))
 		-> .cutter(n)
 	[] - 
 
-	G - General
+	g - General
 	v - Remove empty var().
 
-	A - All conversions except R and F
+	a - All conversions except R and F
 
-	U - Actually update the source file
+	u - Actually update the source file
 	V - Verbose
 )";
 
@@ -67,17 +69,17 @@ function main() {
 
 	COMMAND.remover(1);
 
-	if (OPTIONS.contains("A"))
-		OPTIONS ^= "BSLCPGv[]";
-	var r2a = OPTIONS.contains("R");
-	var forrange = OPTIONS.contains("F");
-	var substr2b = OPTIONS.contains("B");
-	var b1eq2starts = OPTIONS.contains("S");
-	var b2last = OPTIONS.contains("L");
-	var general = OPTIONS.contains("G");
+	if (OPTIONS.contains("a"))
+		OPTIONS ^= "bslcpgv[]";
+	var r2a = OPTIONS.contains("r");
+	var forrange = OPTIONS.count("f");
+	var substr2b = OPTIONS.contains("b");
+	var b1eq2starts = OPTIONS.contains("s");
+	var b2last = OPTIONS.contains("l");
+	var general = OPTIONS.contains("g");
 	var emptyvar = OPTIONS.contains("v");
-	var cutting = OPTIONS.contains("C");
-	var splicing = OPTIONS.contains("P");
+	var cutting = OPTIONS.contains("c");
+	var splicing = OPTIONS.contains("p");
 	var onechar = OPTIONS.contains("[]");
 
 	var verbose = OPTIONS.contains("V");
@@ -104,7 +106,10 @@ function main() {
 
 		// For each text line in the source file
 		// Using var& instead of var allows us to update lines directly into dim txt()
+		int linen = 0;
 		for (var& line : txt) {
+
+			++linen;
 
 			var pos = 0;
 
@@ -173,58 +178,71 @@ function main() {
 			}// r2a
 
 			// r2a - xxx.r(a,b,c,d) -> =xxx(1,b,c) = d;
-			var pattern = "for (var ";
-			if (forrange)
-				pos = line.index(pattern);
-			if (forrange and pos) {
+			if (forrange and line.contains("\tfor (") and not line.contains("; --")) {
 
 				//var line2 = hide_subsyntax(line, pos, ';');
 				var line2 = line;
 				bool fail = false;
 
 				//for (const var dictidn : range(1, ndictids)) {
+				var pattern = "for (var ";
+				pos = line.index(pattern);
+				if (pos) {
+					if (forrange ne 2) {
+						// for (var dictidn = 1
+						var part1 = line2.b(pos).field(";", 1).trim();
+						var varname = part1.field(" ", 3);
+						var starting = part1.field("=", 2, 999).trim();
 
-				// for (var dictidn = 1
-				var part1 = line2.b(pos).field(";", 1).trim();
-				var varname = part1.field(" ", 3);
-				var starting = part1.field("=", 2, 999).trim();
+						// dictidn <= ndictids + 20
+						var part2 = line2.field(";", 2).trim();
+						var varname2 = part2.field(" ", 1).trim();
+						if (varname2 ne varname)
+							fail = true;
+						var compare = part2.field(" ", 2);
+						if (compare ne "<=" and compare ne "le")
+							fail = true;
+						var ending = part2.field(" ", 3, 999);
 
-				// dictidn <= ndictids + 20
-				var part2 = line2.field(";", 2).trim();
-				var varname2 = part2.field(" ", 1).trim();
-				if (varname2 ne varname)
-					fail = true;
-				var compare = part2.field(" ", 2);
-				if (compare ne "<=" and compare ne "le")
-					fail = true;
-				var ending = part2.field(" ", 3, 999);
+						// ++dictidn) {
+						var part3 = line2.field(";", 3).trim();
+						if (part3 ne ("++" ^ varname ^ ") {") and part3 ne (varname ^ "++) {"))
+							fail = true;
 
-				// ++dictidn) {
-				var part3 = line2.field(";", 3).trim();
-				if (part3 ne ("++" ^ varname ^ ") {") and part3 ne (varname ^ "++) {"))
-					fail = true;
+						// Cannot really occur since part3 is checked for ++varname
+						if (starting.isnum() and ending.isnum() and starting > ending) {
+		//					TRACE(line2)
+		//					TRACE(starting)
+		//					TRACE(ending)
+							fail = true;
+						}
 
-				// Cannot really occur since part3 is checked for ++varname
-				if (starting.isnum() and ending.isnum() and starting > ending) {
-//					TRACE(line2)
-//					TRACE(starting)
-//					TRACE(ending)
-					fail = true;
-				}
+						//printl("part1 :", part1.quote());
+						//printl("part2 :", part2.quote());
+						//printl("part3 :", part3.quote());
+						//printl("varname :", varname.quote(), ",", varname2.quote(), "starting :", starting.quote(), "compare :" ^ compare.quote(), "ending :", ending.quote());
+						if (not fail) {
+							line2.paster(pos, 99999, "for (const var " ^ varname ^ " : range(" ^ starting ^ ", " ^ ending ^ ")) {");
+						}
+					}
+				} else if (forrange eq 2) {
+					line2.regex_replacer(
+						R"___(\tfor \((.+?) = (.+?); \1 (<=|le) (.+?); (\+\+\1|\1\+\+)\))___",
+						R"___(\tfor \(\1 : range\(\2, \4\)\))___");
+						//R"___(\tfor \(([a-zA-Z_.0-9]+) =)___",
+						//R"___(QQQ : range\()___");
+	//				if (line.contains("QQQ"))
+	//					TRACE(line);
 
-				//printl("part1 :", part1.quote());
-				//printl("part2 :", part2.quote());
-				//printl("part3 :", part3.quote());
-				//printl("varname :", varname.quote(), ",", varname2.quote(), "starting :", starting.quote(), "compare :" ^ compare.quote(), "ending :", ending.quote());
-				if (not fail) {
-					line2.paster(pos, 99999, "for (const var " ^ varname ^ " : range(" ^ starting ^ ", " ^ ending ^ ")) {");
 				}
 
 				if (not line2.contains(" : range(")) {
 
 					// Failed to convert
-					if (line2.contains(";"))
+					if (line2.contains(";")) {
+						errput(osfilename, ":", linen, " ");
 						errputl('?', line2.replace("\t", "    "));
+					}
 
 				} else {
 
@@ -234,6 +252,7 @@ function main() {
 					if (line2 ne line) {
 						line = line2;
 						replaced = true;
+						output(osfilename, ":", linen, " ");
 						printl('+', line.replace("\t", "    "));
 					}
 				}
