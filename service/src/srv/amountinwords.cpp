4ac783340a9938b92672413101ebd821dab152d6
root <sb2@neosys.com>
2023-04-05 10:51:31 +0000

    * JE/AP: Show amount in words in Arabic, if company language is Arabic or English/Arabic

--- service/src/srv/amountinwords.cpp
+++ service/src/srv/amountinwords.cpp
@@ -14,15 +14,19 @@ var currcode;
 var amount;	 // num
 var language;
 var words;
+var lastwords;
 var millions;
 var thousands;
 var hundreds;
 var unitsx;	 // num
+var subunitsx;  // num
 var text;
 var cents;
 var tens;  // num
 var ones;  // num
 var temp;
+var unitname;
+var subunitname;
 
 function main(in amountfmlanguage, in currcode0) {
 
@@ -48,6 +52,7 @@ function main(in amountfmlanguage, in currcode0) {
 
 	amount	 = amountfmlanguage.f(1);
 	language = amountfmlanguage.f(2);
+	language.ucaser();
 
 	amount.converter("-", "");
 	if (amount == "") {
@@ -57,6 +62,10 @@ function main(in amountfmlanguage, in currcode0) {
 		return "Zero";
 	}
 	words = "";
+
+    if (language.contains("ARABIC")) {
+        goto arabic;
+    }
 
 	if (language == "FRENCH") {
 		goto french;
@@ -119,13 +128,16 @@ function main(in amountfmlanguage, in currcode0) {
 		}
 
 	} else if (amountcurrency.f(2)) {
-		words ^= " " ^ amountcurrency.f(1);
+		//words ^= " " ^ amountcurrency.f(1);
+		words ^= " " ^ amountcurrency.f(1).field("|", 1);
 
 		// subunits
 		if (amount.floor() != amount) {
 			words ^= " and ";
 			words ^= (("." ^ amount.field(".", 2)).oconv("MD" ^ amountcurrency.f(3) ^ "0PZ")).cut(1);
-			words ^= " " ^ amountcurrency.f(2);
+			//words ^= " " ^ amountcurrency.f(2);
+			// Get english currency name
+			words ^= " " ^ amountcurrency.f(2).field("|", 1);
 		}
 
 	} else {
@@ -136,15 +148,204 @@ function main(in amountfmlanguage, in currcode0) {
 		}
 
 		if (amountcurrency) {
-			words ^= " " ^ amountcurrency.f(1);
+			//words ^= " " ^ amountcurrency.f(1);
+			// Get english currency name
+			words ^= " " ^ amountcurrency.f(1).field("|", 1);
 		}
 	}
 
 	return words.trim();
 
 ///////
+arabic:
+///////
+
+	// Warning! arguments in replacer are in wrong order due to arabic left to right
+	// example, to replace X with Y, do replacer(Y, X)
+
+	// Get arabic currency name
+    if (amountcurrency) {
+        unitname    = amountcurrency.f(1).field("|", 2);
+        subunitname = amountcurrency.f(2).field("|", 2);
+
+        if (words == "") {
+            words ^= " " ^ unitname ^ " ";
+        }
+    }
+
+    // Add word "million"
+    millions = amount.field(".", 1);
+    millions.cutter(-6);
+    if (millions) {
+        words ^= amountinwords(millions ^ FM ^ language, "" );
+
+        if (millions == 2) {
+
+			// Special arabic word for two million
+            words ^= " مليونان ";
+
+			// remove the word two, because مليونان is a special word for "two million"
+            words.replacer("اثنان مليونان" , "مليونان");
+            if (words != "")
+                words ^= " و";
+
+        } else {
+            // Everything else e.g 3,4 millions
+            words ^= " مليون ";
+
+            // remove "one" from "one million"
+            words.replacer("واحد مليون" , "مليون");
+            if (words != "")
+                words ^= " و";
+        }
+    }
+
+    // Add word "thousand"
+    thousands = ((amount.mod(1000000)) / 1000).floor();
+    if (thousands) {
+
+        words ^= amountinwords(thousands ^ FM ^ language);
+
+        if (thousands == 2) {
+
+			// Special arabic word for two thousand
+            words ^= " ألفان ";
+
+			// remove the word two, because مليونان is a special word for "two thousand"
+            words.replacer("اثنان ألفان" , "ألفان");
+            if (words != "")
+                words ^= " و";
+
+        } else {
+
+            // Everything else e.g 3,4 thousands
+            words ^= " ألف ";
+
+            // remove "one" from "one thousand"
+            words.replacer("واحد ألف" , "ألف");
+            if (words != "")
+                words ^= " و";
+        }
+    }
+
+    // Add word "hundred"
+    hundreds = ((amount.mod(1000)) / 100).floor();
+    if (hundreds) {
+        words ^= amountinwords(hundreds ^ FM ^ language);
+
+        if (hundreds == 2) {
+
+			// Special arabic word for two hundred
+            words ^= " مائتان";
+
+			// remove the word two, because مليونان is a special word for "two hundred"
+            words.replacer("اثنان مائتان" , "مائتان");
+            if (words != "")
+                words ^= " و";
+
+        } else {
+
+            // Everything else e.g 3,4 hundreds
+            words ^= " مائة";
+
+            // remove "one" from "one hundred"
+            words.replacer("واحد مائة" , "مائة");
+            if (words != "")
+                words ^= " و";
+        }
+    }
+
+    // Get units in words
+    unitsx = "00" ^ amount.mod(1000).floor();
+    text   = "صفر " _VM "واحد " _VM "اثنان " _VM "ثلاثة " _VM "أربعة " _VM "خمسة " _VM "ستة " _VM "سبعة " _VM "ثمانية " _VM "تسعة " _VM "عشرة " _VM "أحد عشر " _VM "اثنا عشر " _VM "ثلاثة عشر " _VM "أربعة عشر " _VM "خمسة عشر " _VM "ستة عشر " _VM "سبعة عشر " _VM "ثمانية عشر " _VM "تسعة عشر " _VM "عشرين " _VM "ثَلاثين " _VM "أربعين " _VM "خمسين " _VM "ستين " _VM "سبعين " _VM "ثمانين " _VM "تسعين ";
+    if (unitsx) {
+        if (unitsx <= 20) {
+            words ^= text.f(1, unitsx + 1);
+        } else {
+
+            tens     = unitsx[-2];
+            ones     = unitsx[-1];
+
+            // ones
+            if (ones != "0") {
+                words ^= text.f(1, 1 + ones);
+            }
+
+            // tens
+            if (tens != "0") {
+                if (words != "" and ones != "0")
+                    words ^= " و";
+                words ^= text.f(1, 19 + tens);
+            }
+        }
+        //Currency unit name in Arabic
+        unitname = amountcurrency.f(1).field("|", 2);
+        if (unitname) {
+            // Move dirhams to end of string
+            words.replacer(unitname, "" );
+            words ^= unitname;
+        }
+    }
+
+    // Get subunits in words
+    if (amount.floor() != amount) {
+        subunitsx = amount.field(".", 2);
+
+        if (subunitsx.len()) {
+
+            if (subunitsx <= 20) {
+                // fils will always start with an "and"
+                words ^= "و ";
+                words ^= text.f(1, subunitsx + 1);
+
+            } else {
+
+                if (subunitsx >= 100) {
+                    // tenth
+                    hundreds = subunitsx[+1];
+                    // hundredth
+                    tens     = subunitsx[+2];
+                    // thousandth
+                    ones     = subunitsx[+3];
+                } else {
+
+                    hundreds = "";
+                    tens     = subunitsx[+1];
+                    ones     = subunitsx[+2];
+                }
+
+                // hundreds
+                if (hundreds and (hundreds != "0")) {
+                    words ^= " و";
+                    words ^= text.f(1, 1 + hundreds);
+                    words ^= " مائة ";
+                }
+
+                // ones
+                if (ones != "0") {
+                    words ^= " و";
+                    words ^= text.f(1, 1 + ones);
+                }
+
+                // tens
+                if (tens and (tens != "0")) {
+                    words ^= " و";
+                    words ^= text.f(1, 19 + tens);
+                }
+            }
+
+            //Currency sub-unit name in Arabic
+            subunitname = amountcurrency.f(2).field("|", 2);
+            //fils
+            words ^= subunitname;
+        }
+    }
+
+    return words.trim();
+
+///////
 french:
-	// /////
+///////
 	// millions
 	millions = amount.field(".", 1);
 	millions.cutter(-6);
