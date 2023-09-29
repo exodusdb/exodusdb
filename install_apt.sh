5c92629e9bda1f8073a2a330c0aa69f8eee43577
neo <sb2@neosys.com>
2023-09-29 11:20:30 +0400

    * SB: install_apt.sh (which first git clones exodus) now just calls install.sh to do the actual work of building and installing

--- install_apt.sh
+++ install_apt.sh
@@ -1,108 +1,105 @@
 #!/bin/bash
 set -euxo pipefail
-
-: ===============================================
-: 0. Building and Installing Exodus Using apt-get
-: ===============================================
 :
-: You can view/run the latest version of this script directly
+: -------------------------------------------------------
+: Download, build and install the full exodus environment
+: -------------------------------------------------------
+:
+: Includes libexodus, exodus cli, c++ compiler and postgresql database.
+:
+: You can view the latest version of this script directly using wget or curl
 :
 : "wget -O - https://raw.githubusercontent.com/exodusdb/exodusdb/master/install_apt.sh | less"
-: "wget -O - https://raw.githubusercontent.com/exodusdb/exodusdb/master/install_apt.sh | bash"
-: or
 : "curl https://raw.githubusercontent.com/exodusdb/exodusdb/master/install_apt.sh | less"
+:
+: or run the latest version directly.
+:
+: "wget -O - https://raw.githubusercontent.com/exodusdb/exodusdb/master/install_apt.sh | bash"
 : "curl https://raw.githubusercontent.com/exodusdb/exodusdb/master/install_apt.sh | bash"
-
 :
 : Installing on:
 :
 	uname -a
+<<<<<<< Updated upstream
 	cat /etc/issue
-
+=======
+	lsb_release -a
+>>>>>>> Stashed changes
 :
 : Tested on:
 :
-: "Ubuntu 23.04 Ubuntu Lunar Lobster"
-: "Ubuntu 22.04 Ubuntu Jammy Jellyfish"
+: "Ubuntu 22.04 Ubuntu Jammy Jellyfish (development branch)"
 : "Ubuntu 20.04.3 LTS"
 :
-: No longer tested:
+: No longer tested - requires C++20
 :
 : "Ubuntu 18.04.1 LTS"
 : "Ubuntu 10.04.2 LTS"
 : "Linux lucid64 2.6.32-31-server" #61-Ubuntu SMP Fri Apr 8 19:44:42 UTC 2011 x86_64 GNU/Linux
 : "Debian GNU/Linux 6.0 \n \l"
 : "Linux debian32 2.6.32-5-686" #1 SMP Wed May 18 07:08:50 UTC 2011 i686 GNU/Linux
-
+<<<<<<< Updated upstream
+:
 :
 : 1. Building and Installing Exodus
 : =================================
 :
-:
-: 'Install into $1, $EXODUSDIR or ~/exodus'
-:
-	EXODUSDIR=${1:-${EXODUSDIR:-~/exodus}}
-
-:
 : Update repos in case building on a pristine installation
 :
 	sudo apt-get update
-
+=======
 :
-: Install dependencies
+: Config
+: ------
 :
+	EXODUSDIR=${1:-${EXODUSDIR:-~/exodus}}
+:
+: Update apt and install git
+: --------------------------
+:
+	sudo apt-get update
+ 	sudo DEBIAN_FRONTEND=noninteractive apt-get -y install git
+>>>>>>> Stashed changes
+:
+: Git clone exodus or refresh if already present
+: ----------------------------------------------
+:
+<<<<<<< Updated upstream
  	sudo DEBIAN_FRONTEND=noninteractive \
 		apt-get -y install \
 		git \
 		cmake \
 		postgresql-server-dev-all \
 		g++ build-essential \
-		libboost-regex-dev libboost-locale-dev
-		# Will be automatically installed
-		# atomic chrono date-time serialization system thread
-		#
-		# was:
-		#libboost-dev libboost-system-dev libboost-regex-dev libboost-thread-dev libboost-locale-dev libboost-chrono-dev libboost-date-time-dev libboost-atomic-dev
-
+		libboost-dev libboost-system-dev libboost-regex-dev libboost-thread-dev libboost-locale-dev libboost-chrono-dev libboost-date-time-dev libboost-atomic-dev
 :
 : Download exodus if not already present
 :
 	cd ~
 	[ ! -d exodus ] && git clone https://github.com/exodusdb/exodusdb exodus
-
 :
 : Refresh git in case reinstalling
 :
-	cd $EXODUSDIR
+	cd ~/exodus
 	git stash
 	git pull
-
 :
-: Config make with all CPUs in parallel
+: Config make with all cpus in parallel
 :
-	cd $EXODUSDIR
-	cmake . -D CMAKE_BUILD_TYPE=${BUILD_TYPE:-RelWithDebInfo}
-
+	cd ~/exodus
+	cmake .
 :
 : Clean all - unless suppressed for speed. Required in case half-built with wrong libs somehow.
 :
-	[ ${NO_CLEAN-x} ] && make clean
+	[ ${NOCLEAN:-} ] || make clean
 	cmake .
 
 :
-: Make all exodus lib, cli and pgexodus with all CPUs in parallel. Hopefully enough memory.
+: Make all exodus lib, cli and pgexodus
 :
 	make -j `nproc`
-
 :
-: Skip full testing unless FORCE_TEST is set
-:
-	[ ${NO_TEST-x} ] && CTEST_OUTPUT_ON_FAILURE=1 CTEST_PARALLEL_LEVEL=`nproc` make test
-
-:
-: Install all exodus lib and cli - Probably into /usr/local ... lib, bin, include/exodus and share/exodus
-:
-: Probably into /usr/local ... lib, bin, include/exodus and share/exodus
+: Install all exodus lib and cli
 :
 	sudo make install
 
@@ -115,7 +112,6 @@ set -euxo pipefail
         #yum -y install postgresql-server
         sudo DEBIAN_FRONTEND=noninteractive \
         apt-get -y install postgresql postgresql-client
-
 :
 : Restart postgres
 :
@@ -126,8 +122,8 @@ set -euxo pipefail
 :
 : make install again to install pgexodus extension and functions, and exodus user and database
 :
-        cd $EXODUSDIR
-        sudo make install
+        cd ~/exodus
+        make install
 
 :
 : 3. Configuring Exodus for Postgres
@@ -141,33 +137,47 @@ set -euxo pipefail
 	user=exodus \
 	password=somesillysecret \
 	> ~/.config/exodus/exodus.cfg
-
 :
 : 4. Add some postgres utility functions
 : ======================================
 :
-: Use an exodus cli program
+: one of the exodus cli programs
 :
 	dict2sql
-
 :
 : 5. Testing Exodus
 : =================
-:
 : Every database requires a lists file in order to store select lists
 :
 	createfile lists || true
 	cd ~
 	testsort
-
 :
 : 6. Programming with Exodus
 : ==========================
-: You must make some change to hello or actually save it, not just quit.
+: you must make some change to hello or actually save it, not just quit
 : edic hello
 : hello
 : compile hello
-
 :
 : 7. Finished $0 $* in $((SECONDS / 60)) minutes and $((SECONDS % 60)) seconds
 : ======================================================================
+=======
+	if [ ! -d $EXODUS_DIR ]; then
+		git clone https://github.com/exodusdb/exodusdb $EXODUS_DIR
+	else
+		cd $EXODUSDIR
+		#git stash
+		git pull || true
+		#git stash pop
+	fi
+:
+: Install all
+: -----------
+:
+	cd $EXODUSDIR
+	./install.sh $*
+:
+: Finished $0 $* in $((SECONDS/60)) mins and $((SECONDS%60)) secs.
+: ----------------------------------------------------------------
+>>>>>>> Stashed changes
