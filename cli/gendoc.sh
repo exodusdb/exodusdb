0b08def67027a5f9689bbda279e9d1934f3431ec
root <sb2@neosys.com>
2025-05-13 14:06:41 +0000

    ~ SB: Add gendoc.sh to generate doc instead of using multiple cmake commands

---
+++ cli/gendoc.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+set -euxo pipefail
+:
+:	$0 $*
+:
+:	Syntax is ./gendoc.sh [GENDOCPATH] [EXODUS_PATH]
+:
+	GENDOC_PATH=${1:-}
+	EXODUS_PATH=${2:-~/exodus/exodus}
+
+	${GENDOC_PATH}gendoc $EXODUS_PATH/{var.h,dim.h,exoprog.h} {m} > $EXODUS_PATH/var.1
+	${GENDOC_PATH}gendoc $EXODUS_PATH/{var.h,dim.h,exoprog.h} {h} > $EXODUS_PATH/var.htm
