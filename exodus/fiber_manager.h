aeff981020c1cfcecb8204063964ee01e375d007
root <sb2@neosys.com>
2025-05-27 11:13:17 +0000

    = SB: Improve doc for run and co_run in man var

--- exodus/fiber_manager.h
+++ exodus/fiber_manager.h
@@ -46,13 +46,13 @@ public:
 	// Templated constructor to accept any callable and its arguments
 
 	// Call any function asynchronously.
-	// Exodus' asynchronous functions run to completion, or they request database i/o, or they call yield(), and their execution is interleaved with other async functions. Since they are not threaded, they are implicitly thread-safe and do not need to use locking when reading or updating shared data.
-	// Exodus' asynchronous functions are fibers akin to threads but without parallel processing. They are extremely fast to setup and switch with complete stack and state.
-	// The initial state of asynchronous functions is "ready, not running". Once the main program calls yield() then one is selected by a round robin scheduler and they begin running and yielding to each other.
+	// Exodus' asynchronous functions run sequentially not simultaneously. Their execution will be interleaved with other async functions while database i/o is pending or when they call yield(). They are implicitly thread-safe. Global state may be changed by other async functions but not simultaneously.
+	// Exodus' asynchronous functions are fibers akin to threads but without parallel processing or OS management. They are extremely fast to setup and and switch between and have a normal stack. They might be called "coprocesses" by comparion with "coroutines" which do not have stacks.
+	// The initial state of an asynchronous function is "ready, not running". Once the main program calls yield() then one of the "ready, not running" async functions is selected by a round robin scheduler and it runs until complete, requests database i/o or yields, and so on.
 	//
 	// `//func add(in a, in b) {
 	//  //    set_run_result(a + b);
-	//	//	return "";
+	//  //	return "";
 	//  //}
 	//  for (var i : range(1, 100)) {
 	//      co_run(&_ExoProgram::add, this, i, 2);
@@ -82,14 +82,17 @@ public:
 	void yield() const;
 
 	// Set a co_run result.
+	// Callable in an async function to provide output collectible by co_run_results().
 	// See co_run for an example.
 	void set_run_result(var data, var message = "") const;
 
-	// A range that can be used in range-based for loops to pick up all co_run results.
+	// A range-based for loop to pick up all co_run results.
 	// See co_run for an example.
 	auto co_run_results() -> std::generator<CoRunResult&>;
 
-	// Get the number of co_runs started.
+	// Get the number of co_runs pending.
+	// Not required if you use co_run_results to collect results.
+	// Calling co_run increases the number and co_run_results reduces the number.
 	auto co_run_count() const -> var {return co_run_count_;}
 
 //    // Calls shutdown, dtor, ctor. Useful to start a new batch of jobs followed by async collection of all run_results.
