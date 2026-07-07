4afe4acbd0aa6f756e4164d62d84878e42035db6
NEOSYS Dev <dev@neosys.com>
2026-07-07 09:13:38 +0000

    Fix decide popup checkbox toggling undone on mouse release
    decide_document_onclick was passing dblclick (false on single click) as the
    checking argument to decide_checkbox_select, forcing uncheck after mouseup.
    
    Skip bubbled clicks on checkbox inputs; only pass explicit forceCheck on
    double-click. Move checkbox handler from mousedown to mouseup so row
    formatting updates on button release.

--- service/www/3/exodus/scripts/client.js
+++ service/www/3/exodus/scripts/client.js
@@ -5849,10 +5849,14 @@ function decide_onload(decide_args) {
 		}
 
 		var oCellxInput = oCellx.getElementsByTagName('input')[0]
-		if (event_handler)
-			//oCellxInput.onclick=event_handler
-			oCellxInput.onmousedown = event_handler
-		//addeventlistener(oCellxInput,'click',event_handler)
+		if (event_handler) {
+			// Checkbox: mouseup so row format/check state updates on release, not press.
+			// Radio: mousedown for immediate select+OK.
+			if (decide_returnmany)
+				oCellxInput.onmouseup = event_handler
+			else
+				oCellxInput.onmousedown = event_handler
+		}
 		oCellxInput.setAttribute('decide_optionno', optionno)
 
 		if (defaultreply && defaultreply.exoduslocate(returnvalue))
@@ -6046,7 +6050,7 @@ function decide_onload(decide_args) {
 		if (!element)
 			element = event.target
 
-		//dont click on checkbox since mousedown will have already done the job
+		// dont handle bubbled click on checkbox; mouseup on the input already did the job
 		if (event.type == 'click' && event.target.type == 'checkbox')
 			return false
 
@@ -6166,8 +6170,13 @@ function decide_onload(decide_args) {
 		return
 	}
 
-	function decide_document_onclick(event, dblclick) {
+	function decide_document_onclick(event, forceCheck) {
 		event = getevent(event)
+
+		// Checkbox input uses its own mouseup handler; ignore bubbled click (was
+		// passing false as 'checking' and undoing the selection).
+		if (event.target && event.target.type == 'checkbox')
+			return exoduscancelevent(event)
 
 		var trtag = getancestor(event.target, 'tr')
 		var element
@@ -6177,8 +6186,12 @@ function decide_onload(decide_args) {
 			return
 		}
 
-		if (decide_returnmany)
-			decide_checkbox_select(event, element, dblclick)
+		if (decide_returnmany) {
+			if (typeof forceCheck === 'boolean')
+				decide_checkbox_select(event, element, forceCheck)
+			else
+				decide_checkbox_select(event, element)
+		}
 		else
 			decide_radio_select(event, element)
 
